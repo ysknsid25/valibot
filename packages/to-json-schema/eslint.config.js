6dfa839a5b0247e0c2acbd0006d217c7095c4f7c
Fabian Hiller <hillerfabian11@gmail.com>
2025-11-20 22:53:50 -0500

    Ignore for eslint.config.js in ESLint configurations

--- packages/to-json-schema/eslint.config.js
+++ packages/to-json-schema/eslint.config.js
@@ -5,6 +5,7 @@ import pluginSecurity from 'eslint-plugin-security';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
+  { ignores: ['eslint.config.js'] },
   eslint.configs.recommended,
   tseslint.configs.strict,
   tseslint.configs.stylistic,
